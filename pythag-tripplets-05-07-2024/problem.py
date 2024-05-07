6a3b59f6218e865b12151d81effbf787e40d60d1
Andrew Blackburn <andrew.blackburn@protonmail.com>
2024-05-07 19:44:39 -0400

    Add pythag triples pseudocode

---
+++ pythag-tripplets-05-07-2024/problem.py
@@ -0,0 +1,14 @@
+def findPythagoreanTriplets(nums):
+  # Fill this in.
+
+  #Get all possible combos of 3 numbers in the array
+  #sort small to large [a, b, c]
+  #see if a^2 b^2 = c^
+
+  #research more about pythat tripplet patterns
+  #I would assume there is a nice pattern we could follow. 
+
+  return True
+
+print (findPythagoreanTriplets([3, 12, 5, 13]))
+# True
